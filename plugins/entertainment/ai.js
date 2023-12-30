75b13d0bcfdcd58a54779ec4449365d9a13c5354
Ndaakid <154701424+Ndaakid@users.noreply.github.com>
2023-12-30 08:57:50 +0000

    ndaa

---
+++ plugins/entertainment/ai.js
@@ -0,0 +1,15 @@
+import { openai } from 'betabotz-tools'
+import { connect } from 'mongoose'
+
+let handler = async (m, { conn, text, command, usedPrefix}) => {
+    if (!text) return m.reply(`Contoh : ${usedPrefix + command} apa itu google?`)
+    let json = await opena
+conn.reply(m.chat, json.result, m)
+}
+handler.help = ['ai'].map(v => v + ' <pertanyaan>')
+handler.tags = ['entertainment']
+handler.command = /^(ai|openai)$/i
+
+handler.premium = true
+
+export default handler
