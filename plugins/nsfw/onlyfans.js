a9e74ca4009807ac060e80514a8523e379c369d5
Ndaakid <154701424+Ndaakid@users.noreply.github.com>
2023-12-30 09:26:51 +0000

    ndaa

--- plugins/nsfw/onlyfans.js
+++ plugins/nsfw/onlyfans.js
@@ -3,7 +3,7 @@ import { pickRandom } from '../../lib/func.js'
 let handler = async (m, { conn, usedPrefix, command }) => {
 		let res = await fetch(`https://raw.githubusercontent.com/Ndaakid/Databasee/main/nsfw/onlyfans.json`)
 		let anu = pickRandom(await res.json())
-        await conn.sendFile(m.chat, anu, null, null, m)
+        await conn.sendFile(m.chat, anu, null, 'dosa tanggung sendiri', m)
 	}
 
 handler.menunsfw = ['onlyfans']
