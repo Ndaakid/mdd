ac207b87958579ff5ecd3b22c345d7320e66a373
Ndaakid <154701424+Ndaakid@users.noreply.github.com>
2023-12-30 18:57:44 +0700

    Update join.js

--- plugins/owner/join.js
+++ plugins/owner/join.js
@@ -3,19 +3,11 @@ let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
 const cooldown = 86400000
 
 let handler = async (m, { conn, text, isOwner }) => {
-	//if (db.data.settings[conn.user.jid].restrict) throw `[ RESTRICT ENABLED ]`
 	let [_, code, expired] = text.match(linkRegex) || []
 	if (!code) throw 'Link invalid'
 	try {
 		let res = await conn.groupAcceptInvite(code)
-		expired = Math.floor(Math.min(999, Math.max(1, isOwner ? isNumber(expired) ? parseInt(expired) : 0 : 3)))
 		m.reply(`Berhasil join grup ${res}`)
-		/*let chats = db.data.chats[res]
-		if (!chats) chats = db.data.chats[res] = {}
-		if (expired) {
-			chats.expired = +new Date() + expired * cooldown
-			chats.joindate = new Date * 1
-			chats.joincd = expired * cooldown*/
 		}
 	} catch (e) {
 		console.log(e)
@@ -26,6 +18,8 @@ let handler = async (m, { conn, text, isOwner }) => {
 handler.menuowner = ['join <chat.whatsapp.com>']
 handler.tags = ['information']
 handler.command = /^(join)$/i
+
+handler.limit = true
 
 export default handler
 
