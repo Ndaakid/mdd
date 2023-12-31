d6492f02d31fae49d713fa278961cb00bf32cac3
Ndaakid <154701424+Ndaakid@users.noreply.github.com>
2023-12-31 09:33:44 +0000

    update

--- plugins/sepuh/claims.js
+++ plugins/sepuh/claims.js
@@ -3,9 +3,7 @@ import db from '../../lib/database.js'
 let handler = async (m, {conn, text, command, usedPrefix}) => {
  m.reply(`tes aja`)
 }
-handler.help = ['']
-handler.tags = ['']
-handler.command = /^()$/i
+handler.command = /^(tesaja)$/i
 
 handler.sepuh = true
 
